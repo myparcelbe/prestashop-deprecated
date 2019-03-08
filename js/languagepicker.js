b4279edbdc59f3bef87fa57d3311077546066d14
Michael Dekker <git@michaeldekker.nl>
2019-03-08 15:51:18 +0100

    Direct language links (#lang=NL)

--- js/languagepicker.js
+++ js/languagepicker.js
@@ -1,12 +1,20 @@
+
 $( document ).ready(function() {
-    $('#NL').click();
+    if (['#lang=FR', '#lang=NL'].indexOf(window.location.hash) > -1) {
+        $('#' + window.location.hash.split('=')[1].toUpperCase()).click();
+    } else {
+        $('#NL').click();
+    }
 });
 
 /*
-* Load the correct language
-*/
+ * Load the correct language
+ */
 $(document).on("click",".languagepicker > li", function () {
     var clickedLanguage = $(this).attr('id'); /* Get the id of the selected language */
+    if (typeof window.history !== 'undefined' && typeof window.history.replaceState === 'function') {
+        window.history.replaceState(null,null,'#lang=' + clickedLanguage);
+    }
     var lowerCase = clickedLanguage.toLowerCase();
 
     $(this).prependTo('ul.languagepicker'); /* set the selected language at first option */
